5ac576be18a50f7f5d4dd1e10add385e9a2e95b5
ZaydAlnabulsi <zaydalnabulsi@gmail.com>
2025-03-28 01:31:48 -0400

    Add files via upload

---
+++ geometrydashclone/scripts/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+const SPEED = 25000.0
+const JUMP_VELOCITY = -1200.0
+
+var gravity = 4100
+
+func _physics_process(delta):
+	#Gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		$PlayerBody.rotation_degrees += 360 * delta
+	else :
+		var module = int($PlayerBody.rotation_degrees) % 90;
+		if module > 45 :
+			$PlayerBody.rotation_degrees += (90 - module)
+		else :
+			$PlayerBody.rotation_degrees -= module
+
+	#Jump
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	velocity.x = delta * SPEED
+
+	move_and_slide()
