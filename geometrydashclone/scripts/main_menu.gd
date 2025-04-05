dce1ee19bb8ab0b21c5d7f121d3ba050b43bed70
aaronkelly5 <112659591+aaronkelly5@users.noreply.github.com>
2025-04-05 13:42:51 -0400

    Exit button quits game

--- geometrydashclone/scripts/main_menu.gd
+++ geometrydashclone/scripts/main_menu.gd
@@ -5,3 +5,7 @@ func _on_new_game_button_pressed() -> void:
 
 func _on_settings_button_pressed() -> void:
 	Global.scene_manager.change_scene("res://scenes/settings_menu.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
