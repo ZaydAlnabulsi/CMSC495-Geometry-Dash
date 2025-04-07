cd005354a01e5e2e05894033620a5577b42b166a
aaronkelly5 <112659591+aaronkelly5@users.noreply.github.com>
2025-04-07 18:51:29 -0400

    Allow user to start a new game or return to main menu upon death

--- geometrydashclone/scripts/gameplay/player_death.gd
+++ geometrydashclone/scripts/gameplay/player_death.gd
@@ -1,11 +1,8 @@
 extends Area2D
 
-##	Variable to hold the GamePlay node
-@onready var gameplay = get_node("/root/SceneManager/Gameplay")
-
 ##	Function that causes player's death on collision
 func _on_body_entered(body):
 	if body.is_in_group("kill") :
-		gameplay.display_death_overlay()
+		get_node("/root/SceneManager").get_child(0).display_death_overlay()
 		$"..".death()
-		self.queue_free() 
+		self.queue_free()
