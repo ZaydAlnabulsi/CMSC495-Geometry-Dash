19d9559e2bf6e6325d8158f0e56f95b03b9f00c1
aaronkelly5 <112659591+aaronkelly5@users.noreply.github.com>
2025-04-22 11:45:59 -0400

    fixed back button on level select

--- geometrydashclone/scenes/level_select.gd
+++ geometrydashclone/scenes/level_select.gd
@@ -9,3 +9,7 @@ func _on_level_1_button_pressed() -> void:
 func _on_test_level_button_pressed() -> void:
 	Global.set_scene_change_root(2)
 	Global.scene_manager.change_scene("res://scenes/gameplay.tscn")
+
+
+func _on_back_button_pressed() -> void:
+	Global.scene_manager.change_scene("res://scenes/main_menu.tscn")
