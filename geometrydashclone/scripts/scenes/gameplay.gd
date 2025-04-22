74c1e47e796a826abd439162e4668d45b2b51f19
aaronkelly5 <112659591+aaronkelly5@users.noreply.github.com>
2025-04-21 20:29:51 -0400

    Modified pause menu.  Added return to main menu button

--- geometrydashclone/scripts/scenes/gameplay.gd
+++ geometrydashclone/scripts/scenes/gameplay.gd
@@ -87,3 +87,7 @@ func _on_new_game_button_pressed() -> void:
 
 func _on_return_to_main_menu_button_pressed() -> void:
 	Global.scene_manager.change_scene("res://scenes/main_menu.tscn")
+
+func _on_exit_to_main_menu_button_pressed() -> void:
+	get_tree().paused = false
+	Global.scene_manager.change_scene("res://scenes/main_menu.tscn")
